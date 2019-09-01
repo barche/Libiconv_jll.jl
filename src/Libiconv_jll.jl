c2be3368fce947cdd5e3a5aef1920d7c87f1ba1c
Elliot Saba <staticfloat@gmail.com>
2019-09-01 02:24:52 -0700

    Libiconv_jll build 1.16.0+0

---
+++ src/Libiconv_jll.jl
@@ -0,0 +1,26 @@
+module Libiconv_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("94ce4f54-9a6c-5748-9c1c-f9c7231a4531"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Libiconv", artifacts_toml) for e in artifacts["Libiconv"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Libiconv; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Libiconv_jll
